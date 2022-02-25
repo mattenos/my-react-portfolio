f353bdbc56055c158635b5948b4cb4f0fdd871a8
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-25 10:51:25 -0500

    Completed the Resume and Contact components.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/AboutMe/About';
 import Projects from './components/MyProjects/Projects';
-import Skills from './components/MySkill/Skills'
+import Skills from './components/MySkill/Skills';
+import Contact from './components/ContactMe/Contact';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Contact />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ function Header() {
         <div class= 'text-white bg-gray-900 body-font'>
             <nav>
             <ul class= 'flex justify-end space-x-4 p-4 '>
-                <li><a href="/portfolio">Portfolio</a></li>
-                <li><a href="/about">About Me</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/resume">Resume</a></li>
+                <li><a href="/#projects">Portfolio</a></li>
+                <li><a href="/#about">About Me</a></li>
+                <li><a href="/#contact">Contact</a></li>
+                <li> <a href="https://drive.google.com/file/d/1_UqAugoxErOHFgPX_6VSo3GKIIBXfGf4/view?usp=sharing" download="Matt_Enos_Tech_Resume">Resume</a></li>
             </ul>
             </nav>
         </div>
