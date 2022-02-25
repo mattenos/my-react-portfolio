68482114a1fa4f968c8a613de0c12c5e6fd6cedc
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-25 10:18:37 -0500

    Added additional components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/AboutMe/About';
+import Projects from './components/MyProjects/Projects';
+import Skills from './components/MySkill/Skills'
 
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
+      <Projects />
+      <Skills />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+    {
+      title: "Cinemagic",
+      subtitle: "Full Stack App with API Integration",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "../images/cinemagic.png",
+      link: "https://cinemajik.herokuapp.com/",
+    },
+    {
+      title: "The Ultimate Urban Dictionary",
+      subtitle: "Fetch API and Local Storage",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://rwashi690.github.io/Urban_Dictionary_and_Memes/",
+    },
+    {
+      title: "JavaScript Quiz",
+      subtitle: "JavaScript and Local Storage",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://mattenos.github.io/coding-quiz/",
+    },
+    {
+      title: "Random Password Generator",
+      subtitle: "JavaScript",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://mattenos.github.io/password-generator/",
+    },
+  ];
+
+export const skills = ['HTML/CSS', 'JavaScript', 'React', 'Git/GitHub', 'MERN', 'MySQL']
