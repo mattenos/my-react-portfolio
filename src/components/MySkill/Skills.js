127b41b75f78fdff49e73327b1f91f133aa9e1d9
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-25 13:26:12 -0500

    Activated the Contact component. Edited some text content.

--- src/components/MySkill/Skills.js
+++ src/components/MySkill/Skills.js
@@ -9,9 +9,7 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            Below are listed the many different languages and technologies that I work with and am continuing to improve.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
