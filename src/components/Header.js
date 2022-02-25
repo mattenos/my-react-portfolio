ac24bca9c8b249431aca971da55312b94d4bab89
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-25 14:59:55 -0500

    Updated Contact link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
             <ul class= 'flex justify-end space-x-4 p-4 '>
                 <li><a href="/#projects">Portfolio</a></li>
                 <li><a href="/#about">About Me</a></li>
-                <li><a href="/#email">Contact</a></li>
+                <li><a href="/#contact">Contact</a></li>
                 <li><a href="https://drive.google.com/file/d/1_UqAugoxErOHFgPX_6VSo3GKIIBXfGf4/view?usp=sharing" download="Matt_Enos_Tech_Resume">Resume</a></li>
             </ul>
             </nav>
