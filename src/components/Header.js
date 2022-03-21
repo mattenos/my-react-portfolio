1057634143cc78ad410492279959a8b3eddd2544
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-03-21 15:09:15 -0400

    working on links.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ function Header() {
         <div class= 'text-white bg-gray-900 body-font'>
             <nav>
             <ul class= 'flex justify-end space-x-4 p-4 '>
-                <li><a href="my-react-portfolio/#projects">Portfolio</a></li>
-                <li><a href="my-react-portfolio/#about">About Me</a></li>
-                <li><a href="my-react-portfolio/#contact">Contact</a></li>
+                <li><a href="/projects">Portfolio</a></li>
+                <li><a href="/about">About Me</a></li>
+                <li><a href="/contact">Contact</a></li>
                 <li><a href="https://drive.google.com/file/d/10rpgqFy0s0wwuDEXxLYe4Fqf1rCzCmNL/view?usp=sharing" download="Matt_Enos_Tech_Resume">Resume</a></li>
             </ul>
             </nav>
