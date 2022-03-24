d7db069c2ef4ea4b7a390dd42d9fd82ef51c84f3
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-03-24 10:04:00 -0400

    Revise contact component and remove form.

--- src/components/ContactMe/Contact.js
+++ src/components/ContactMe/Contact.js
@@ -10,36 +10,8 @@ export default function Contact() {
                     Let's Connect!
                 </h2>
                 <p className="leading-relaxed mb-5">
-                    Please email me with any inquiries.
+                Please email me at <span class="font-bold">matthewenos1@gmail.com</span> or call me on my mobile at <span class="font-bold">678-595-6456</span> with any inquiries.
                 </p>
-                <div className="relative mb-4">
-                    <label htmlFor="email" className="leading-7 text-sm text-slate-800">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="w-full bg-gray-400 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label
-                        htmlFor="message"
-                        className="leading-7 text-sm text-slate-800">
-                        Message
-                    </label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        className="w-full bg-gray-400 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Submit
-                </button>
             </form>
         </section>
     );
